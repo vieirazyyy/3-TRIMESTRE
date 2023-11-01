4f1aaa28c4a24803cc4c830269e1b81f82b69fcd
vieirazyyy <132680194+vieirazyyy@users.noreply.github.com>
2023-11-01 08:23:54 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function tocaSom(idElementoAudio){
+    document.querySelector(idElementoAudio).play();
+}
+const listaDeTeclas= document.querySelectorAll(".tecla");
+
+let contador=0;
+while (contador<listaDeTeclas.length){
+    const efeito=listaDeTeclas[contador].classList[1];
+    const idAudio='#som_'+efeito;
+    listaDeTeclas[contador].onclick=function(){
+        tocaSom(idAudio);
+    }
+    contador= contador+1;
+    console.log(contador);
+}
